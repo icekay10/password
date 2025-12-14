5d7043e373837ddd2f6aedb1ae517a7d72fdc0e3
icekay10 <isatakamara1001@gmail.com>
2025-12-14 14:44:53 +0200

    Update: description of changes

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import styles from './Homepage.module.css';
+import styles from './HomePage.module.css';
 
 // SEO Metadata (Next.js App Router)
 export const metadata = {
